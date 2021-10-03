ff6d8feb42e3240346d3bb9c2e398778894df59a
Vidi <valeriivolkov91@gmail.com>
2021-10-03 21:38:04 +0300

    Initial commit.

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[][] arr = new String[][]{{"1", "2", "3", "4"}, {"1", "2", "3", "4"}, {"1", "2", "3", "4"}, {"1", "2", "3", "4"}};
+    }
+}
